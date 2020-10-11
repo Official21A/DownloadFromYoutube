7e72629921c7d3fc4f84528fbffb94d99519eaa1
Official21A <officialamirhossein21@gmail.com>
2020-10-11 07:38:03 +0330

    Source by url

--- script.py
+++ script.py
@@ -6,4 +6,9 @@ youtube = pytube.YouTube(url)
 
 video = youtube.streams.first()
 
+print(video.title, video.video_id, video.age_restricted)
+
+for i in video.streams.all():
+	print(i)
+
 video.download()
